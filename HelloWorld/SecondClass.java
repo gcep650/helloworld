860417017d63c244b1ff778468409abe29105517
Gabriel <gcep650@learner.dvusd.org>
2019-08-12 13:51:31 -0700

    new

---
+++ HelloWorld/SecondClass.java
@@ -0,0 +1,31 @@
+package HelloWorld;
+
+
+/**
+ * Write a description of class SecondClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SecondClass
+{
+
+    /**
+     * Constructor for objects of class SecondClass
+     */
+    public static void main(String args[])
+    {
+        System.out.println("Hello");
+        int x = 10;
+        for (int i = 0; i<10; i++)
+        {
+            System.out.println(x);
+            x++;
+        }
+    }
+    
+    public SecondClass()
+    {
+
+    }
+}
---
+++ HelloWorld/dt.java
@@ -0,0 +1,18 @@
+package HelloWorld;
+
+public class dt
+{
+    public static void main(String[] args)
+    {
+        int x = 3;
+        byte by = 0;
+        String s = "bob";
+        double d = 5.5;
+        float f = .1f;
+        boolean b = true;
+        long l = 54;
+        char c = 3;
+        System.out.println("Hello " + b);
+        
+    }
+}
