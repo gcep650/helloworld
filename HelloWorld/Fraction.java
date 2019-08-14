146c66be4d3e24984d84c957fc05231fcfe753e4
Gabriel <gcep650@learner.dvusd.org>
2019-08-14 13:58:24 -0700

    more functions to fraction class. new class readlinetest except there's not anything in it yet.

--- HelloWorld/Fraction.java
+++ HelloWorld/Fraction.java
@@ -7,18 +7,44 @@ public class Fraction
     
     public Fraction(int num, int denom)
     {
+        
+        m_num = num / euclid(num, denom);
+        m_denom = denom / euclid(num, denom);
+    }
+    
+    public Fraction(int num)
+    {
         m_num = num;
-        m_denom = denom;
+        m_denom = 1;
+    }
+    
+    public Fraction recip()
+    {
+        return new Fraction(m_denom, m_num);
     }
     
     public Fraction add(Fraction other)
     {
-        return null;
+        return new Fraction(
+        m_num * other.m_denom + m_denom * other.m_num,
+        m_denom * other.m_denom);
+    }
+    
+    public Fraction subtract(Fraction other)
+    {
+        return new Fraction(
+        m_num * other.m_denom - m_denom * other.m_num,
+        m_denom * other.m_denom);
     }
     
     public Fraction multiply(Fraction other)
     {
         return new Fraction(m_num * other.m_num, m_denom * other.m_denom);
+    }
+    
+    public Fraction divide(Fraction other)
+    {
+        return multiply(other.recip());
     }
     
     public String toString()
@@ -30,6 +56,22 @@ public class Fraction
     {
         Fraction f1 = new Fraction(1,2);
         Fraction f2 = new Fraction(3,4);
-        System.out.println(f1.multiply(f2));
+        System.out.println(f1.add(f2));
     }
+    
+    public int euclid(int x, int y) {
+		if (x == 0 || y == 0) {
+			return 1;
+		}
+		if (x < y) {
+			int t = x;
+			x = y;
+			y = t;
+		}
+		if (x % y == 0) {
+			return y;
+		} else {
+			return euclid(y, x % y);
+		}
+	}
 }
