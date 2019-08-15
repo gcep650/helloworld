2d6178a3f3877a7e5478ed96dee02b7e2ce9457a
Gabriel <gcep650@learner.dvusd.org>
2019-08-15 08:36:07 -0700

    ReadLineTest actually does something: reading lines!

--- HelloWorld/ReadLineTest.java
+++ HelloWorld/ReadLineTest.java
@@ -1,35 +1,18 @@
 package HelloWorld;
+import java.util.Scanner;
 
-
-/**
- * Write a description of class ReadLineTest here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class ReadLineTest
 {
-    // instance variables - replace the example below with your own
-    private int x;
 
-    /**
-     * Constructor for objects of class ReadLineTest
-     */
-    public ReadLineTest()
+    public static void main(String[] args)
     {
-        // initialise instance variables
-        x = 0;
+         Scanner in = new Scanner(System.in);
+         
+         String s = in.nextLine();
+         System.out.println("You typed: " + s);
+         
+         int i = in.nextInt();
+         System.out.println("Number you typed: " + i);
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
