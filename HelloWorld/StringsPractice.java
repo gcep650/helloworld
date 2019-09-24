efdb3794b647fc6bd7b21410548b116640d31484
Gabriel <gcep650@learner.dvusd.org>
2019-09-24 14:11:25 -0700

    strings practice

--- HelloWorld/StringsPractice.java
+++ HelloWorld/StringsPractice.java
@@ -6,12 +6,14 @@ public class StringsPractice
 
     public static void main(String[] args)
     {
+        System.out.println("accomplishment".substring(2, 4));
         String s;
         String temp = "";
-        String longest = "null";
+        String longest = "";
         Scanner in = new Scanner(System.in);
         System.out.print("Enter a sentence: ");
         s = in.nextLine();
+        String[] words = s.split(" ");
         int tempx = 0;
         int length = 0;
         while (tempx > -1)
@@ -23,6 +25,7 @@ public class StringsPractice
             if (templ > length) {length = templ; longest = temp;}
             s = s.substring(tempx + 1);
         }
-        System.out.println(longest);
+        System.out.println("Number of words: " + words.length);
+        System.out.println("The longest word: " + longest);
     }
 }
